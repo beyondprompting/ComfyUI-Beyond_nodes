026c935ee44c7f7863ea60aab3ff1ebe63b81735
beyond <beyondprompting@gmail.com>
2025-12-02 14:28:30 -0300

    Refactor

--- nodes/Logic.py
+++ nodes/Logic.py
@@ -229,37 +229,3 @@ class DebugPrint:
         print(ANY)
         return {}
 
-from ..common.types import BOOLEAN, STRING, CATEGORY, any
-
-class CSwitchBooleanAny:
-    def __init__(self):
-        pass
-
-    @classmethod
-    def INPUT_TYPES(cls):
-        return {
-            "required": {
-                "on_true": (any, {"lazy": True}),
-                "on_false": (any, {"lazy": True}),
-                "boolean": BOOLEAN,
-            }
-        }
-
-    CATEGORY = "Beyond Nodes/Switching"
-    # CATEGORY = CATEGORY.MAIN.value + CATEGORY.SWITCH.value
-    
-    RETURN_TYPES = (any,)
-
-    FUNCTION = "execute"
-
-    def check_lazy_status(self, on_true=None, on_false=None, boolean=True):
-        needed = "on_true" if boolean else "on_false"
-        return [needed]
-
-    def execute(self, on_true, on_false, boolean=True):
-        logger.debug("Any switch: " + str(boolean))
-
-        if boolean:
-            return (on_true,)
-        else:
-            return (on_false,)
